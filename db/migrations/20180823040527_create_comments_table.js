3dbf86b2b72836af3c6da321bb9e3d11834aa473
Ben Deltenre <benjdelt@gmail.com>
2018-08-23 04:07:01 +0000

    add comments migration

---
+++ db/migrations/20180823040527_create_comments_table.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('comments', function (table) {
+    table.increments();
+    table.text('text');
+    table.timestamp('timestamp');
+    table.integer('resources_id').references('resources.id');
+    table.integer('user_id').references('users.id');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('comments');
+};  
