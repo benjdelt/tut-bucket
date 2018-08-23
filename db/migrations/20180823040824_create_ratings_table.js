f23b44ac4abaaf5a78479ec4f0dde33944048e0e
Ben Deltenre <benjdelt@gmail.com>
2018-08-23 04:09:24 +0000

    Added ratings table migration

---
+++ db/migrations/20180823040824_create_ratings_table.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('ratings', function (table) {
+    table.increments();
+    table.integer('value');
+    table.integer('resources_id').references('resources.id');
+    table.integer('user_id').references('users.id');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('ratings');
+};  
