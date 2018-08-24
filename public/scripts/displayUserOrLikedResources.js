3030153c65a2d35f10b17cd7ef84232433b56f87
Jotham Yelle <jothamyelle@hotmail.com>
2018-08-24 21:03:51 +0000

    Added the functions that make ajax requests to grab the users posts and users liked posts

---
+++ public/scripts/displayUserOrLikedResources.js
@@ -0,0 +1,43 @@
+function displayUserResources() {
+  let url = $(location).attr('href');
+  let userID = url[url.length - 1];
+
+  $.ajax({
+    method: "GET",
+    url: `/resources/users/${userID}`
+  }).done((resources) => {
+    for (item of resources) {
+      var result = `<div class="col-lg-3 col-md-4 col-xs-6">
+        <a href="#" class="d-block mb-4 h-100">
+          <img class="img-fluid img-thumbnail custom-img" src="http://placehold.it/400x300" alt="">
+          <div class="carousel-caption">
+            <h5>${item.title}</h5>
+          </div>
+        </a>
+      </div>`;
+      $("#collections").append(result);
+    }
+  });
+}
+
+function displayLikedResources() {
+  let url = $(location).attr('href');
+  let userID = url[url.length - 1];
+
+  $.ajax({
+    method: "GET",
+    url: `/resources/liked/${userID}`
+  }).done((resources) => {
+    for (item of resources) {
+      var result = `<div class="col-lg-3 col-md-4 col-xs-6">
+        <a href="#" class="d-block mb-4 h-100">
+          <img class="img-fluid img-thumbnail custom-img" src="http://placehold.it/400x300" alt="">
+          <div class="carousel-caption">
+            <h5>${item.title}</h5>
+          </div>
+        </a>
+      </div>`;
+      $("#collections").append(result);
+    }
+  });
+}
--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,8 @@ const router  = express.Router();
 const dataHelpers = require('../lib/users-data-helpers');
 
 module.exports = (knex) => {
-  let user = {};
   router.get("/", (req, res) => {
+    let user = {};
     knex
       .select("*")
       .from("users")
@@ -23,9 +23,22 @@ module.exports = (knex) => {
     });
   });
 
- 
-  router.get("/feature", (req, res) => {
-    res.render("user_page");
+
+  router.get("/:id", (req, res) => {
+    let user = {};
+    knex
+      .select("*")
+      .from("users")
+      .where({id: req.params.id})
+      .then((result) => {
+        let templateVars = {
+          name: result[0].name,
+          email: result[0].email,
+          avatar: result[0].avatar_url
+        }
+        console.log(templateVars);
+        res.render("user_page", templateVars);
+    });
   });
 
   return router;
