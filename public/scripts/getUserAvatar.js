32e2204b9a20ad3910ea366fa904ce39882163c1
Jotham Yelle <jothamyelle@hotmail.com>
2018-08-24 03:26:14 +0000

    Added first half of user page

--- public/scripts/getUserAvatar.js
+++ public/scripts/getUserAvatar.js
@@ -1,16 +1,3 @@
-function randomColor() {
-  let colors = [
-    '#ff3232', //red
-    '#49ff53', //green
-    '#75aaff', //blue
-    '#b949ff', //purple
-    '#ff4979', //pink
-    '#fff249', //yellow
-    '#ff8e49'  //orange
-  ];
-  return colors[Math.floor(Math.random() * Math.floor(colors.length))];
-}
-
 $.ajax("/resources/categories", {method:"GET"})
   .then((categoriesObject) => {
     for (category in categoriesObject) {
--- routes/users.js
+++ routes/users.js
@@ -6,15 +6,21 @@ const router  = express.Router();
 const dataHelpers = require('../lib/users-data-helpers');
 
 module.exports = (knex) => {
-
+  let user = {};
   router.get("/", (req, res) => {
-    // knex
-    //   .select("*")
-    //   .from("users")
-    //   .then((results) => {
-    //     res.json(results);
-    // });
-    
+    knex
+      .select("*")
+      .from("users")
+      .where({id: 1})
+      .then((result) => {
+        let templateVars = {
+          name: result[0].name,
+          email: result[0].email,
+          avatar: result[0].avatar_url
+        }
+        console.log(templateVars);
+        res.render("user_page", templateVars);
+    });
   });
 
  
