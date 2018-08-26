6650c4921fc96150c73a3bf0b2c8681291f084dc
Jotham Yelle <jothamyelle@hotmail.com>
2018-08-26 22:29:51 +0000

    Usernames are now displaying in the single resource

--- public/scripts/getSingleResourceTemplate.js
+++ public/scripts/getSingleResourceTemplate.js
@@ -1,101 +1,106 @@
 function getSingleResourceTemplate(category, select_resource) {
-    var singleResource = `
-      <div>
-        <main class="container">
-          <div class="row">
-            <div class="col-lg-2">
-              <a href="/">Back</a>
-            </div>
-            <div class="col-lg-8">
-              <h2 data-resourceId ="${select_resource.id}" class="mt-4">${select_resource.title}</h2>
-              <p id="resourceCategory">${category}</p>
-              <div class="d-flex justify-content-between">
-                <h4> username </h4>
-                <a id="resourceURL" href="${select_resource.url}">${select_resource.url}</a>
+  let userNum = $("#userPage").attr("href").replace("/", "");
+  $.ajax({method: "GET", url: `/users/${userNum}/user`})
+    .then((users) => {
+      let user = users[0];
+      var singleResource = `
+        <div>
+          <main class="container">
+            <div class="row">
+              <div class="col-lg-2">
+                <a href="/">Back</a>
               </div>
-              <img class="img-fluid rounded mx-auto d-block" src="${select_resource.image_url}"/>
-              <p>Posted on ${select_resource.timestamp}</p>
-              <div class="d-flex justify-content-between">
-               <div id="ratingsContainer" class="btn-group btn-group-toggle" data-toggle="buttons">
-                  <label  data-resource-id="${select_resource.id}" class="btn btn-success" id="A">
-                    <input type="radio" name="ratings"> A
-                  </label>
-                  <label  data-resource-id="${select_resource.id}" class="btn btn-success" id="B">
-                    <input type="radio" name="ratings"> B
-                  </label>
-                  <label  data-resource-id="${select_resource.id}" class="btn btn-warning" id="C+">
-                    <input type="radio" name="ratings"> C+
-                  </label>
-                  <label  data-resource-id="${select_resource.id}" class="btn btn-warning" id="C">
-                      <input type="radio" name="ratings"> C
-                  </label>
-                  <label  data-resource-id="${select_resource.id}" class="btn btn-warning" id="C-">
-                      <input type="radio" name="ratings"> C-
+              <div class="col-lg-8">
+                <h2 data-resourceId ="${select_resource.id}" class="mt-4">${select_resource.title}</h2>
+                <p id="resourceCategory">${category}</p>
+                <div class="d-flex justify-content-between">
+                  <h4>${user.name}</h4>
+                  <a id="resourceURL" href="${select_resource.url}">${select_resource.url}</a>
+                </div>
+                <img class="img-fluid rounded mx-auto d-block" src="${select_resource.image_url}"/>
+                <p>Posted on ${select_resource.timestamp}</p>
+                <div class="d-flex justify-content-between">
+                <div id="ratingsContainer" class="btn-group btn-group-toggle" data-toggle="buttons">
+                    <label  data-resource-id="${select_resource.id}" class="btn btn-success" id="A">
+                      <input type="radio" name="ratings"> A
                     </label>
-                  <label  data-resource-id="${select_resource.id}" class="btn btn-danger" id="D">
-                    <input type="radio" name="ratings"> D
-                  </label>
-                  <label  data-resource-id="${select_resource.id}" class="btn btn-danger" id="F">
-                      <input type="radio" name="ratings"> F
-                  </label>
+                    <label  data-resource-id="${select_resource.id}" class="btn btn-success" id="B">
+                      <input type="radio" name="ratings"> B
+                    </label>
+                    <label  data-resource-id="${select_resource.id}" class="btn btn-warning" id="C+">
+                      <input type="radio" name="ratings"> C+
+                    </label>
+                    <label  data-resource-id="${select_resource.id}" class="btn btn-warning" id="C">
+                        <input type="radio" name="ratings"> C
+                    </label>
+                    <label  data-resource-id="${select_resource.id}" class="btn btn-warning" id="C-">
+                        <input type="radio" name="ratings"> C-
+                      </label>
+                    <label  data-resource-id="${select_resource.id}" class="btn btn-danger" id="D">
+                      <input type="radio" name="ratings"> D
+                    </label>
+                    <label  data-resource-id="${select_resource.id}" class="btn btn-danger" id="F">
+                        <input type="radio" name="ratings"> F
+                    </label>
+                  </div>
+                  <div id="like">
+                      <button type="button" data-resource-id="${select_resource.id}" class="btn btn-outline-info ">Like</button>
+                  </div>
                 </div>
-                <div id="like">
-                    <button type="button" data-resource-id="${select_resource.id}" class="btn btn-outline-info ">Like</button>
+                <div class="d-flex justify-content-between">
+                  <div id=currentRating>
+                    <p>Current rating: <span>`;
+      getRatings(select_resource.id)
+      .then((currentRating) => {   
+        singleResource += currentRating;
+        singleResource += 
+                        `</span>
+                    </p>
+                  </div>
+                  <div id="numberOfLikes">
+                    <p>`;
+      getLikes(select_resource.id)
+      .then((numLikes) => {
+        singleResource += numLikes[0].count;
+        singleResource += 
+                    `</p>
+                  </div>
+                </div>
+                <p id="resourceDescription" class="lead">${select_resource.description}</p>
+                <div class="card my-4">
+                  <h5 class="card-header">Leave a Comment:</h5>
+                  <div class="card-body">
+                    <form id="commentForm" data-resource-id="${select_resource.id}">
+                      <div class="form-group">
+                        <textarea id="commentText" class="form-control" rows="3" name="commentContent"></textarea>
+                      </div>
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                  </div>
                 </div>
               </div>
-              <div class="d-flex justify-content-between">
-                <div id=currentRating>
-                  <p>Current rating: <span>`;
-    getRatings(select_resource.id)
-    .then((currentRating) => {   
-      singleResource += currentRating;
-      singleResource += 
-                      `</span>
-                  </p>
-                </div>
-                <div id="numberOfLikes">
-                  <p>`;
-    getLikes(select_resource.id)
-    .then((numLikes) => {
-      singleResource += numLikes[0].count;
-      singleResource += 
-                  `</p>
-                </div>
+              <div class="col-lg-2">
+                <a id="editResource" href="#">Edit</a>
               </div>
-              <p id="resourceDescription" class="lead">${select_resource.description}</p>
-              <div class="card my-4">
-                <h5 class="card-header">Leave a Comment:</h5>
-                <div class="card-body">
-                  <form id="commentForm" data-resource-id="${select_resource.id}">
-                    <div class="form-group">
-                      <textarea id="commentText" class="form-control" rows="3" name="commentContent"></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                  </form>
-                </div>
-              </div>
+              <div id="commentArea">`;
+          getComments(select_resource.id)
+          .then((comment) => {
+            if(comment) {
+              singleResource += comment; 
+            }
+            singleResource += 
+              `</div>
             </div>
-            <div class="col-lg-2">
-              <a id="editResource" href="#">Edit</a>
-            </div>
-            <div id="commentArea">`;
-        getComments(select_resource.id)
-        .then((comment) => {
-          if(comment) {
-            singleResource += comment; 
-          }
-          singleResource += 
-            `</div>
-          </div>
-        </main>
-      </div>`;
+          </main>
+        </div>`;
 
-      $('#navbar, #nick, #jotham').hide();
-      $('#singeResource').html(singleResource);
-      getLikesClicks();
-      getRatingsClicks();
-      getCommentsClicks();
-      }) 
+        $('#nick, #jotham').hide();
+        $('#singeResource').html(singleResource);
+        getLikesClicks();
+        getRatingsClicks();
+        getCommentsClicks();
+        }) 
+      })
     })
   })
 }
